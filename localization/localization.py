516ab2c4b8aab1e57eec2a154da4601b1a8bfd6b
Juan Rios <juandarr@gmail.com>
2019-01-29 23:58:52 -0500

    feat: added function move which updates the probability distribution based on the move step and the transition probabilities

--- localization/localization.py
+++ localization/localization.py
@@ -14,11 +14,21 @@ Author: Juan David Rios
 
 p=[0.2, 0.2, 0.2, 0.2, 0.2]
 world=['green', 'red', 'red', 'green', 'green']
-Z = 'red'
+measurements = ['red', 'green']
+motions = [1,1]
 pHit = 0.6
 pMiss = 0.2
 
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
+prob = [pUndershot, pExact, pOvershoot]
+
 def sense(p, Z):
+    """
+    Changes the probability distribution based on the observation Z
+    and the measure probability pHit, pMiss
+    """
     q=[]
     for i in range(len(p)):
         hit = (Z == world[i])
@@ -27,5 +37,14 @@ def sense(p, Z):
     for i in range(len(q)):
         q[i] /= total
     return q
-    
+
+def move(p, U):
+    """
+    Moves the agent and updates the probability distribution with a convolution
+    """
+    q = []
+    for i in range(len(p)):
+        q.append(p[i-U+1]*prob[0]+p[i-U]*prob[1]+p[i-U-1]*prob[2])
+    return q
+
 print(sense(p,Z))
